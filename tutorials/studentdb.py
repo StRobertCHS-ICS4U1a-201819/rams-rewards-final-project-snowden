d225d21c17edddc840ebabb3f9579f034bd31217
littlelookre2 <connorfor@gmail.com>
2018-12-14 18:19:30 -0500

    screen switch

--- tutorials/studentdb.py
+++ tutorials/studentdb.py
@@ -16,14 +16,57 @@ class StudentDB(BoxLayout):
     student_list = ObjectProperty()
 
     def submit_student(self):
-        pass
+        student_name = self.first_name_text_input.text + " " + self.last_name_text_input.text
+
+        # Add the student to the ListView
+        self.student_list.adapter.data.extend([student_name])
+
+        # Reset the ListView
+        self.student_list._trigger_reset_populate()
 
     def delete_student(self, *args):
-        pass
+        if self.student_list.adapter.selection:
+            # Get the text from the item selected
+            selection = self.student_list.adapter.selection[0].text
+
+            # Remove the matching item
+            self.student_list.adapter.data.remove(selection)
+
+            # Reset the ListView
+            self.student_list._trigger_reset_populate()
+
+    def delete_student(self, *args):
+
+        # If a list item is selected
+        if self.student_list.adapter.selection:
+
+            # Get the text from the item selected
+            selection = self.student_list.adapter.selection[0].text
+
+            # Remove the matching item
+            self.student_list.adapter.data.remove(selection)
+
+            # Reset the ListView
+            self.student_list._trigger_reset_populate()
 
 
     def replace_student(self, *args):
-        pass
+        # If a list item is selected
+        if self.student_list.adapter.selection:
+            # Get the text from the item selected
+            selection = self.student_list.adapter.selection[0].text
+
+            # Remove the matching item
+            self.student_list.adapter.data.remove(selection)
+
+            # Get the student name from the TextInputs
+            student_name = self.first_name_text_input.text + " " + self.last_name_text_input.text
+
+            # Add the updated data to the list
+            self.student_list.adapter.data.extend([student_name])
+
+            # Reset the ListView
+            self.student_list._trigger_reset_populate()
 
 
 
