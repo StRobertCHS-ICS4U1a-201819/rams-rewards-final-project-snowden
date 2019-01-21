70ec7d29b95880e651e5f79ec29a287b539ac975
littlelookre2 <connorfor@gmail.com>
2019-01-21 16:34:18 -0500

    data

---
+++ RRSA/login.py
@@ -0,0 +1,32 @@
+
+from kivy.app import App
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class LoginPage(Screen):
+    def verify_credentials(self):
+        if self.ids["login"].text == "username" and self.ids["passw"].text == "password":
+            self.manager.current = "user"
+
+class UserPage(Screen):
+    pass
+
+
+class ScreenManagement(ScreenManager):
+    pass
+
+screen_manager = ScreenManager()
+
+
+screen_manager.add_widget(LoginPage(name="login_page"))
+screen_manager.add_widget(UserPage(name="user_page"))
+
+
+kv_file = Builder.load_file('login.kv')
+
+class LoginApp(App):
+    def builder(self):
+        return kv_file
+
+if __name__ == '__main__':
+    LoginApp().run()
---
+++ RRSA/student.py
@@ -0,0 +1,60 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+root = Builder.load_string("""
+PageLayout:
+    BoxLayout:
+        canvas:
+            Color:
+                rgba: 216/255., 195/255., 88/255., 1
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        orientation: 'vertical'
+        Label:
+            size_hint_y: None
+            height: 1.5 * self.texture_size[1]
+            text: 'student history'
+        Button:
+            text: 'points'
+            on_press: print("student points")
+    BoxLayout:
+        orientation: 'vertical'
+        canvas:
+            Color:
+                rgba: 109/255., 8/255., 57/255., 1
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        Label:
+            text: 'Dong'
+        Label:
+            text: 'Connor'
+        Label: 
+            text: 'student id' 
+        Label: 
+            text: 'homeroom'
+    BoxLayout:
+        canvas:
+            Color:
+                rgba: 37/255., 39/255., 30/255., 1
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        cols: 1
+        Button:
+            text: '0000000'
+            on_press: print("test")
+        AsyncImage:
+            source: 'https://i.redd.it/lr720nuui4z11.png'
+""")
+
+
+class KivyTut2App(App):
+    def build(self):
+        return root
+
+
+sample_app = KivyTut2App()
+
+sample_app.run()
